d6b19e12cc9a4fea9653842141190f46795bdc3d
CMRWebDev <crosenberg690@gmail.com>
2015-10-07 18:15:30 -0400

    jquery-animation-complete

--- js/app.js
+++ js/app.js
@@ -12,13 +12,13 @@ $(document).ready(function() {
     $('.ryu-ready').hide();
     $('.ryu-throwing').show();
     $('.hadouken').finish().show().animate(
-  			{'left': '1020px'},
-  			500,
-  		function() {
-    		$(this).hide();
-    		$(this).css('left', '520px');
-  		}
-	);
+        {'left': '1020px'},
+        500,
+      function() {
+        $(this).hide();
+        $(this).css('left', '520px');
+      }
+  );
 
     // play hadouken sound
     // show hadouken and animate it to the right of the screen
@@ -28,10 +28,26 @@ $(document).ready(function() {
     $('.ryu-ready').show();
     // ryu goes back to his ready position
   });
+  
+  $(document).keydown(function (e) {
+    if (e.keyCode == 88) {
+    $('.ryu-ready').hide();
+    $('.ryu-cool').show();
+  } 
+    });
+  $(document).keyup(function (e) {
+    if (e.keyCode == 88) {
+    $('.ryu-ready').show();
+    $('.ryu-cool').hide();
+    }
+    });
 });
 
 function playHadouken () {
-	$('#hadouken-sound')[0].volume = 0.5;
-	$('#hadouken-sound')[0].load();
-	$('#hadouken-sound')[0].play();
+  $('#hadouken-sound')[0].volume = 0.5;
+  $('#hadouken-sound')[0].load();
+  $('#hadouken-sound')[0].play();
 }
+
+
+
